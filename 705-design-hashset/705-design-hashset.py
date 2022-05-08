3d2a438fc94c9399b7cc18e1c8bff8e9d5c4581c
Gautam Yadav <gautamyadav8989@gmail.com>
2022-05-08 22:22:36 +0530

    Time: 166 ms (89.97%), Space: 18.7 MB (74.37%) - LeetHub

--- 705-design-hashset/705-design-hashset.py
+++ 705-design-hashset/705-design-hashset.py
@@ -15,6 +15,43 @@ class MyHashSet:
             return True
         return False
 
+# class List:
+#     def __init__(self,val):
+#         self.next=None
+#         self.val=val
+
+# class MyHashSet:
+
+#     def __init__(self):
+#         self.head=None
+#         self.temp=None
+
+#     def add(self, key: int) -> None:
+#         node=List(key)
+#         if self.head==None:
+#             self.head=node
+#             self.temp=node
+#         elif self.contains(key)==False:
+#             self.temp.next=node
+#             self.temp=self.temp.next
+
+#     def remove(self, key: int) -> None:
+#         if self.contains(key):
+#             curr=self.head
+#             while curr.next and curr.next.val!=key:
+#                 curr=curr.next
+#             if curr==self.head:
+#                 self.head=None
+#             else:
+#                 curr.next=curr.next.next
+    
+#     def contains(self, key: int) -> bool:
+#         curr=self.head
+#         while curr:
+#             if curr.val==key:
+#                 return True
+#             curr=curr.next
+#         return False
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
