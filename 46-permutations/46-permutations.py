c608c707ba4ececcb51a6dd1395c19817620c72d
Gautam Yadav <gautamyadav8989@gmail.com>
2022-05-12 22:43:28 +0530

    Time: 93 ms (5.31%), Space: 14.1 MB (59.38%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        allsol=list()
+        self.backtrack(nums,allsol,list(),dict())
+        return allsol
+    
+    def backtrack(self,nums,allsol,currsol,vis):
+        if len(currsol)==len(nums):
+            allsol.append(currsol[:])
+            return
+        
+        for i in range(len(nums)):
+            if nums[i] not in vis:
+                currsol.append(nums[i])
+                vis[nums[i]]=1
+                self.backtrack(nums,allsol,currsol,vis)
+                currsol.pop()
+                del vis[nums[i]]
