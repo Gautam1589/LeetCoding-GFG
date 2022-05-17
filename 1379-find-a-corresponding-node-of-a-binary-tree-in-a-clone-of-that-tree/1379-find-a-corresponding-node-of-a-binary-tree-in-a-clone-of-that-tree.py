18d7d5cc8e9a27b9461099747c79a26c1fe81443
Gautam Yadav <gautamyadav8989@gmail.com>
2022-05-17 12:00:19 +0530

    Time: 783 ms (48.32%), Space: 23.9 MB (94.29%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -7,9 +7,10 @@
 
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-        self.__ans=0
-        self.dfs1(original,cloned,target)
-        return self.__ans
+        #self.__ans=0
+        #self.dfs1(original,cloned,target)
+        #return self.__ans
+        return self.dfs2(original,cloned,target)
     
     def dfs1(self,original,cloned,target):
         if original==None or cloned==None:
@@ -21,5 +22,18 @@ class Solution:
         
         self.dfs1(original.left,cloned.left,target)
         self.dfs1(original.right,cloned.right,target)
+    
+    def dfs2(self,original,cloned,target):
+        if original==None or cloned==None:
+            return None
+        
+        if original.val==target.val:
+            return cloned
+        
+        left=self.dfs2(original.left,cloned.left,target)
+        if left!=None:
+            return left
+        right=self.dfs2(original.right,cloned.right,target)
+        return right
     
         
