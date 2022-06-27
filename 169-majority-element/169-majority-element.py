e44cb371c6a98cb170d291f03e733b9ee23c1f2d
Gautam Yadav <gautamyadav8989@gmail.com>
2022-06-27 11:48:29 +0530

    Time: 221 ms (61.39%), Space: 15.5 MB (34.50%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,16 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         #hash map
-        d={}
-        for i in nums:
-            d[i]=d.get(i,0)+1
+#         d={}
+#         for i in nums:
+#             d[i]=d.get(i,0)+1
         
-        n=len(nums)//2
-        for i in d:
-            if d[i]>n:
-                return i
+#         n=len(nums)//2
+#         for i in d:
+#             if d[i]>n:
+#                 return i
             
-        #brute force
+        #Moore algorithm
         c=1
         m=nums[0]
         for i in nums:
@@ -21,5 +21,9 @@ class Solution:
             if c==0:
                 m=i
                 c=1
-        return m
+        cnt=0
+        for i in nums:
+            if i==m:
+                cnt+=1
+        return m if cnt>len(nums)//2 else -1
         
