2b995a889ff2834eebf35fca229d6311b52bc20f
Gautam Yadav <gautamyadav8989@gmail.com>
2022-07-11 11:53:07 +0530

    Added solution - LeetHub

--- Anagram - GFG/anagram.py
+++ Anagram - GFG/anagram.py
@@ -9,17 +9,35 @@ class Solution:
         d1={}
         for i in a:
             d1[i]=d1.get(i,0)+1
-        d2={}
         for i in b:
-            d2[i]=d2.get(i,0)+1
-            
-        if len(d1)!=len(d2):
-            return False
+            if i in d1:
+                d1[i]+=1
+            else:
+                return False
         
+        if len(a)!=len(b):
+            return False
+            
         for i in d1:
-            if i not in d2 or d1[i]!=d2[i]:
+            if d1[i]&1:
                 return False
         return True
+        
+        #O(N) time and O(2*N) space
+        # d1={}
+        # for i in a:
+        #     d1[i]=d1.get(i,0)+1
+        # d2={}
+        # for i in b:
+        #     d2[i]=d2.get(i,0)+1
+            
+        # if len(d1)!=len(d2):
+        #     return False
+        
+        # for i in d1:
+        #     if i not in d2 or d1[i]!=d2[i]:
+        #         return False
+        # return True
 
 #{ 
 #  Driver Code Starts
