dd4ce279fa0183b9402899f32c27c041339bf9b5
Gautam Yadav <gautamyadav8989@gmail.com>
2022-07-13 19:13:13 +0530

    Time: 202 ms (55.89%), Space: 15.2 MB (33.23%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,5 +1,21 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        #4th
+        def predicate(mid,nums):
+            if mid==nums[mid]:
+                return False
+            else:
+                return True
+            
+        nums.sort()
+        beg,end=0,len(nums)-1
+        while beg<end:
+            mid=(beg+end)//2
+            if predicate(mid,nums):
+                end=mid
+            else:
+                beg=mid+1
+                
         #3rd
         nums.sort()
         for i in range(len(nums)):
