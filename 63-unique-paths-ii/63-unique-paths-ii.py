9abd47ce101dd0a3c2d0458b16eda670968aef1f
Gautam Yadav <gautamyadav8989@gmail.com>
2022-08-03 18:15:44 +0530

    Time: 43 ms (94.65%), Space: 14.1 MB (18.95%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -1,45 +1,25 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        self.dp={}
         m=len(obstacleGrid)
         n=len(obstacleGrid[0])
-        #memoisation
-        return self.solve(0,0,m,n,obstacleGrid) #O(mn) time
-        #top-to-down
-        return self.table(m,n) #O(mn) time 
-        
-    def solve(self,i,j,m,n,grid):
+        return self.memoise(0,0,obstacleGrid,m,n,{})
+    
+    def memoise(self,i,j,matrix,m,n,dp):
         if i>=m or j>=n:
-            self.dp[(i,j)]=0
+            dp[(i,j)]=0
             return 0
         
-        if grid[i][j]==1:
-            self.dp[(i,j)]=0
+        if matrix[i][j]==1:
+            dp[(i,j)]=0
             return 0
         
         if i==m-1 and j==n-1:
-            self.dp[(i,j)]=1
+            dp[(i,j)]=1
             return 1
         
-        if (i,j) in self.dp:
-            return self.dp[(i,j)]
+        if (i,j) in dp:
+            return dp[(i,j)]
         
-        if i>=m or j>=n:
-            self.dp[(i,j)]=0
-            return 0
+        dp[(i,j)]=self.memoise(i+1,j,matrix,m,n,dp)+self.memoise(i,j+1,matrix,m,n,dp)
+        return dp[(i,j)]
         
-        #if i+1<m and grid[i+1][j]==0:
-        ans=self.solve(i+1,j,m,n,grid)
-        #if j+1<n and grid[i][j+1]==0:
-        ans+=self.solve(i,j+1,m,n,grid)
-        self.dp[(i,j)]=ans
-        return self.dp[(i,j)]
-    
-    def table(self,m,n):
-        dp=[[1]*n for j in range(m)]
-        
-        for i in range(1,m):
-            for j in range(1,n):
-                dp[i][j]=dp[i-1][j]+dp[i][j-1]
-                
-        return dp[m-1][n-1]
