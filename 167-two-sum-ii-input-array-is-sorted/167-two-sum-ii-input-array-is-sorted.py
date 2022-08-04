c0a7efc3e0de37b881d2c1755d6a85784c823036
Gautam Yadav <gautamyadav8989@gmail.com>
2022-08-04 18:14:32 +0530

    Time: 249 ms (22.57%), Space: 15.1 MB (10.94%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        i = 0
-        j = len(numbers) -1
+        #return self.recursive(0,len(numbers)-1,target,numbers)
+        #return self.optimize(0,len(numbers)-1,target,numbers)
+        return self.two_pointer(0,len(numbers)-1,target,numbers)
         
+    #O(N) time O(1) space
+    def two_pointer(self,i,j,target,numbers):    
         while i<j:
             s = numbers[i] + numbers[j]
             if s == target:
@@ -11,7 +14,32 @@ class Solution:
             if s > target:
                 j-=1
             else:
-               i+=1 
-        
+                i+=1 
         return []
         
+    #O(2^N) time O(1) space O(N) stack space
+    def recursive(self,i,j,target,numbers):
+        if i==j:
+            return [-1,-1]
+        
+        if numbers[i]+numbers[j]==target:
+            return [i+1,j+1]
+        
+        ans=self.recursive(i+1,j,target,numbers)
+        if ans[0]!=-1:
+            return ans
+        return self.recursive(i,j-1,target,numbers)
+    
+    #O(N) time O(1) space O(N) stack space
+    def optimize(self,i,j,target,numbers):
+        if i==j:
+            return [-1,-1]
+        
+        if numbers[i]+numbers[j]==target:
+            return [i+1,j+1]
+        
+        if numbers[i]+numbers[j]<target:
+            return self.recursive(i+1,j,target,numbers)
+        else:
+            return self.recursive(i,j-1,target,numbers)
+    
