65fe5dbb5488af94d06fa5e18b4277b7fcc87137
Gautam Yadav <gautamyadav8989@gmail.com>
2022-08-04 12:01:43 +0530

    Time: 96 ms (56.97%), Space: 15.8 MB (55.07%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.py
+++ 260-single-number-iii/260-single-number-iii.py
@@ -1,5 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
+        #O(1) space
+        x=0
+        for i in nums:
+            x^=i
+        d=x&-x
+        y1=0
+        for i in nums:
+            if i&d!=0:
+                y1^=i
+        y2=x^y1
+        return [y1,y2]
+    
+        
+        #O(N) time and O(N) space
         d={}
         for i in nums:
             d[i]=d.get(i,0)+1
