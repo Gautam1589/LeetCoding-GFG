61ece819d5a97dd794c6a9c7b8081dcee303400b
Gautam Yadav <gautamyadav8989@gmail.com>
2022-08-04 18:19:56 +0530

    Time: 208 ms (47.81%), Space: 15.1 MB (10.94%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -2,6 +2,7 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         #return self.recursive(0,len(numbers)-1,target,numbers)
         #return self.optimize(0,len(numbers)-1,target,numbers)
+        #return self.memoise(0,len(numbers)-1,target,numbers,{})
         return self.two_pointer(0,len(numbers)-1,target,numbers)
         
     #O(N) time O(1) space
@@ -29,6 +30,26 @@ class Solution:
         if ans[0]!=-1:
             return ans
         return self.recursive(i,j-1,target,numbers)
+    
+    #O(N^2) time O(N^2) space O(N) stack space
+    def memoise(self,i,j,target,numbers,dp):
+        if (i,j) in dp:
+            return dp[(i,j)]
+        
+        if i==j:
+            dp[(i,j)]=[-1,-1]
+            return [-1,-1]
+        
+        if numbers[i]+numbers[j]==target:
+            dp[(i,j)]=[i+1,j+1]
+            return [i+1,j+1]
+        
+        ans=self.memoise(i+1,j,target,numbers,dp)
+        if ans[0]!=-1:
+            dp[(i,j)]=ans
+            return ans
+        dp[(i,j)]=self.memoise(i,j-1,target,numbers,dp)
+        return dp[(i,j)]
     
     #O(N) time O(1) space O(N) stack space
     def optimize(self,i,j,target,numbers):
