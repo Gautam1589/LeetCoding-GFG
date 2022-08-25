bf6d597e8a58da7e672cdc97c4e304ef81046b7d
Gautam Yadav <gautamyadav8989@gmail.com>
2022-08-25 12:04:43 +0530

    Time: 3125 ms (5.01%), Space: 140.2 MB (6.94%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp1={}
+        dp2={}
+        return self.foo(0,dp1,dp2,prices)
+    
+    def foo(self,i,dp1,dp2,prices):
+        if i in dp1:
+            return dp1[i]
+        
+        if i==len(prices):
+            dp1[i]=0
+            return 0
+        
+        notbuy=self.foo(i+1,dp1,dp2,prices)
+        buy=self.g(i+1,dp1,dp2,prices)-prices[i]
+        dp1[i]=max(notbuy,buy)
+        return dp1[i]
+    
+    def g(self,i,dp1,dp2,prices):
+        if i in dp2:
+            return dp2[i]
+        
+        if i==len(prices):
+            dp2[i]=prices[len(prices)-1]
+            return dp2[i]
+        
+        dp2[i]=max(self.g(i+1,dp1,dp2,prices),prices[i])
+        return dp2[i]
+            
+        
+        
